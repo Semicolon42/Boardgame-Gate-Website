3d8f3ca2d5e78a83d660a687e482cba7df5edc18
Brendan Mehaffy <semicolon42@gmail.com>
2025-09-21 14:04:40 -0700

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import {lazy, Suspense} from 'react'
+import {ErrorBoundary, type FallbackProps} from 'react-error-boundary'
+import {Route, Routes} from 'react-router'
+import {LoadingOrError} from '@/components/LoadingOrError'
+import {Gallery} from '@/pages/Gallery'
+import PlayerHand from './card_components/PlayerHand'
+
+
+const Details = lazy(async () =>
+	import('@/pages/Details').then(m => ({default: m.Details}))
+)
+
+function renderError({error}: FallbackProps) {
+	return <LoadingOrError error={error} />
+}
+
+export function App() {
+	return (
+		<ErrorBoundary fallbackRender={renderError}>
+			<Suspense fallback={<LoadingOrError />}>
+				<Routes>
+					<Route element={<PlayerHand />} index={true} />
+					<Route element={<Gallery />} path='gallery' />
+					<Route element={<Details />} path=':fruitName' />
+				</Routes>
+			</Suspense>
+		</ErrorBoundary>
+	)
+}
---
+++ src/GateComponents/Data/PlayerCards.ts
@@ -0,0 +1,44 @@
+export interface iCitizenCard {
+	id: number,
+	name: string,
+	image: string,
+
+	cost: number,
+  actionCoins: number
+	actionRepair: number
+	actionCalm: number
+	actionFight: number
+	actionBonusId?: string
+	actionBonusText?: string
+}
+
+export const CITIZEN_CARD_LIST: iCitizenCard[] = [
+	{
+		id: 0,
+		name: "Generic",
+		image: "generic",
+
+		cost: 3,
+		actionCoins: 1,
+		actionRepair: 1,
+		actionCalm: 1,
+		actionFight: 1,  
+	}
+]
+
+export function GET_CITIZEN_CARD(id: number): iCitizenCard {
+	const cardNotFound: iCitizenCard = {
+		id,
+		name: "Not Found",
+		image: "not_found",
+
+		cost: 3,
+		actionCoins: 1,
+		actionRepair: 1,
+		actionCalm: 1,
+		actionFight: 1,
+		actionBonusText: "id="+id 
+	}
+
+	return CITIZEN_CARD_LIST.find((it)=>it.id===id) ?? cardNotFound
+}
